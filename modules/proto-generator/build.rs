0cb34d29d96ee28be162ca10e912f5d7778a9639
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-02-01 17:51:52 +0400

    setup base schemas generation for proto file

--- modules/proto-generator/build.rs
+++ modules/proto-generator/build.rs
@@ -1,5 +1,17 @@
-fn main() {
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   println!("cargo:rerun-if-changed=.env");
   println!("cargo:rerun-if-changed=build.rs");
   println!("cargo:rerun-if-changed=src/config.toml");
+
+  let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+
+  tonic_prost_build::configure()
+    .file_descriptor_set_path(out_dir.join("proto_descriptor.bin"))
+    .build_server(true)
+    .build_client(true)
+    .compile_protos(&["proto/blog.proto"], &["proto/blog"])?;
+  Ok(())
 }
