894c9ea28cee88fcecad889c717ce3b4e017039e
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-06-28 23:16:39 +0400

    return correct error for wrong credentials on login

--- modules/blog-server/src/application/auth_service.rs
+++ modules/blog-server/src/application/auth_service.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use crate::application::error::ApplicationError;
 use crate::data::user_repository::UserRepository;
 use crate::domain::user::User;
-use crate::infrastructure::jwt::{JwtService, hash_password, verify_password};
+use crate::infrastructure::jwt::{hash_password, verify_password, JwtService};
 
 #[derive(Clone)]
 pub struct AuthService<R: UserRepository + 'static> {
@@ -56,7 +56,9 @@ where
     email: &str,
     password: &str,
   ) -> Result<String, ApplicationError> {
-    let user = self.get_by_email(&email.to_lowercase()).await?;
+    let Ok(user) = self.get_by_email(&email.to_lowercase()).await else {
+      return Err(ApplicationError::Unauthorized);
+    };
 
     let password_valid = verify_password(password, &user.password_hash)
       .map_err(|_| ApplicationError::Unauthorized)?;
